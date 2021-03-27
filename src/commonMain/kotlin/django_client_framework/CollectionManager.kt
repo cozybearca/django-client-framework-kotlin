96cb07614ceb324507d516e19e5234feef1c93b7
erhss <shreejanp@gmail.com>
2021-03-26 23:42:02 -0400

    changes
    changes

--- src/commonMain/kotlin/django_client_framework/CollectionManager.kt
+++ src/commonMain/kotlin/django_client_framework/CollectionManager.kt
@@ -1,12 +1,19 @@
 package django_client_framework
 
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
 data class Pagination(
     val limit: Int = 50,
     val page: Int = 1,
-    val order_by: String? = null
+    val order_by: String? = "id"
 )
 
-class CollectionManager<T : Model> {
+class CollectionManager {
+    companion object {
+        const val WEBLINK = "https://cozybear.ca"
+    }
+
     /**
      * .page() returns the search results filtered with the query dictionary.
      * If any key of the dictionary is not a valid query key, an InvalidQueryKeyError is thrown.
@@ -14,18 +21,41 @@ class CollectionManager<T : Model> {
      * Example:
      *      val page = Product.objects.page(
      *          mapOf(
-     *              "id__in" to listOf(1,2),
-     *              "name" to "xxyy"
+     *              "id__in" to listOf(1,2), id is 1 or 2..
+     *              "barcode" to "xxyy"        -->
      *          ),
      *          Pagination(limit=50, page=2, order_by: "id")
      *      )
      *
      * It sends a GET request with the query parameters:
-     *      GET /api/v1/product?id__in[]=1&id__in[]=2&name=xxyy&_limit=50&_page=2&_order_by=id
+     *      GET /api/v1/product?id__in[]=1&id__in[]=2&barcode=xxyy&_limit=50&_page=2&_order_by=id
+     *
+     *      i.e. --> https://cozybear.ca/api/v1/product?id__in[]=538&id__in[]=559&_limit=50&_page=1&_order_by=id
+     *
+     *      //-> [] represents array type
      *
      */
-    suspend fun page(query: Map<String, Any?> = emptyMap(), page: Pagination = Pagination()): PageResult<T> {
-        throw NotImplementedError()
+    fun page(itm: String = "product", query: Map<String, Any?> = emptyMap(), page: Pagination = Pagination()): String {
+
+        var pageLink = "$WEBLINK/api/v1/$itm?";
+        for ((k,v) in query){
+            if ((k.takeLast(4)) == "__in"){
+                if (v is List<*>){
+                    for (item in v){
+                        pageLink += "$k[]=$item&";
+                    }
+                }
+            }
+            else{
+                pageLink += "$k=$v&"
+            }
+            println(pageLink)
+        }
+        pageLink += "_limit=${page.limit}&_page=${page.page}&_order_by=${page.order_by}"
+        println(pageLink)
+        val x = Requests()
+        var S = x.runFiles(pageLink)
+        return S.trimIndent()
     }
 
     /**
@@ -45,7 +75,7 @@ class CollectionManager<T : Model> {
      * It sends a GET request with the query parameters:
      *      GET /api/v1/product?id__in[]=1&id__in[]=2&name=xxyy
      */
-    suspend fun get(query: Map<String, Any?>): ObjectManager<T> {
+    suspend fun get(query: Map<String, Any?>): String {
         throw NotImplementedError()
     }
 
@@ -64,7 +94,7 @@ class CollectionManager<T : Model> {
      * It sends a POST request with JSON:
      *      POST /api/v1/product {"name": "xxyy", "price": null}
      */
-    suspend fun create(query: Map<String, Any?>): ObjectManager<T> {
+    suspend fun create(query: Map<String, Any?>): String {
         throw NotImplementedError()
     }
 
@@ -87,7 +117,7 @@ class CollectionManager<T : Model> {
      * If the object does not exist, then sends a POST request with JSON:
      *      POST /api/v1/product {"name": "xxyy", "price": null}
      */
-    suspend fun get_or_create(query: Map<String, Any?>, defaults: Map<String, Any?> = emptyMap()): ObjectManager<T> {
+    suspend fun get_or_create(query: Map<String, Any?>, defaults: Map<String, Any?> = emptyMap()): String {
         throw NotImplementedError()
     }
 
@@ -114,7 +144,11 @@ class CollectionManager<T : Model> {
      * If the object does not exist, then sends a POST request with JSON:
      *      POST /api/v1/product {"name": "xxyy", "price": null}
      */
-    suspend fun update_or_create(query: Map<String, Any?>, defaults: Map<String, Any?> = emptyMap()): ObjectManager<T> {
+    suspend fun update_or_create(query: Map<String, Any?>, defaults: Map<String, Any?> = emptyMap()): String {
         throw NotImplementedError()
+    }
+
+    inline fun<reified T> convert(json: String): T {
+        return Json.decodeFromString<T>(json)
     }
 }
